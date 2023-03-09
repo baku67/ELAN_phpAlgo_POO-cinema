7c9881237be12852c40391379ee9409f7a69f913
baku67 <basile08@hotmail.fr>
2023-03-09 10:41:15 +0100

    Fin requètes de selection SQL

--- database/cinema_bdd_requetes.sql
+++ database/cinema_bdd_requetes.sql
@@ -1,5 +1,5 @@
 -- Exercice a
-SELECT movie_title, movie_frenchPublishDate, movie_length, CONCAT(person_firstName, " ", person_lastName) AS "réalisateur"
+SELECT movie_title, YEAR(movie_frenchPublishDate), movie_length, CONCAT(person_firstName, " ", person_lastName) AS "réalisateur"
 FROM movie m
 INNER JOIN director d ON m.director_id = d.director_id
 INNER JOIN person p ON p.person_id = d.person_id
@@ -7,16 +7,16 @@ INNER JOIN person p ON p.person_id = d.person_id
 -- Exercice b
 SELECT movie_title 
 FROM movie m 
-WHERE (movie_length > 135)
+WHERE movie_length > 135
 ORDER BY movie_length DESC
 
 -- Exercice c
-SELECT movie_title, movie_frenchPublishDate
+SELECT movie_title, YEAR(movie_frenchPublishDate)
 FROM movie m 
-WHERE (director_id = 1)
+WHERE director_id = 1
 
 -- Exercice d
-SELECT COUNT(movie_id) AS "nbr_films", movieGenre_label
+SELECT COUNT(movie_id) AS nbr_films, movieGenre_label
 FROM moviegenrelist m
 INNER JOIN movie_genre mg ON m.movieGenre_id = mg.movieGenre_id
 GROUP BY mg.movieGenre_id
@@ -56,11 +56,11 @@ INNER JOIN person p ON p.person_id = a.person_id
 WHERE (c.actor_id = 1)
 ORDER BY movie_frenchPublishDate DESC
 
--- (!) Exerice i (HS)
+-- (!) Exerice i 
 SELECT movie_title AS "titre film"
 FROM movie m
-WHERE ( DATEDIFF(YEAR, m.movie_frenchPublishDate, DATE(NOW()) ) > 5 )
-ORDER BY ( DATEDIFF(YEAR, m.movie_frenchPublishDate, DATE(NOW()) ) ) ASC 
+WHERE YEAR(NOW()) - YEAR(m.movie_frenchPublishDate) < 50
+ORDER BY m.movie_id
 
 -- Exercice j 
 SELECT COUNT(actor_id) AS "Nbr d'hommes", person_gender AS "sexe"
@@ -68,5 +68,16 @@ FROM actor
 INNER JOIN person ON person.person_id = actor.person_id
 GROUP BY person_gender
 
--- Exercice k
+-- Exercice k (DateDiff OK) 
+SELECT ROUND((DATEDIFF(NOW(), person_birthDate)/360)) AS "Age", CONCAT(person_firstName, " ", person_lastName) AS "Acteurs >= 50ans"
+FROM actor a
+INNER JOIN person p ON p.person_id = a.person_id
+WHERE (ROUND((DATEDIFF(NOW(), person_birthDate)/360)) > 50 )
 
+-- Exercice l
+SELECT CONCAT(person_firstName, " ", person_lastName) AS "Acteur", COUNT(c.actor_id) AS "NbrFilms"
+FROM person p 
+INNER JOIN actor a ON a.person_id = p.person_id
+INNER JOIN casting c ON c.actor_id = a.actor_id
+GROUP BY p.person_id
+HAVING COUNT(c.actor_id) >= 3 
